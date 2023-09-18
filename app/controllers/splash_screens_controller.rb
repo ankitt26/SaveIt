e939bac9f5f7645ac55a5252a1a515eec86be95b
Ankit <ankit26kushwa@gmail.com>
2023-09-19 04:55:50 +0530

    Show splash screen without authenticate user

--- app/controllers/splash_screens_controller.rb
+++ app/controllers/splash_screens_controller.rb
@@ -1,4 +1,4 @@
 class SplashScreensController < ApplicationController
-  def index; end
-  # skip_before_action :authenticate_user!
+   skip_before_action :authenticate_user!
+   def index; end
 end
