cf4cfdf21883af88e79985cbc0e77d9c612c8cd6
Ankit <ankit26kushwa@gmail.com>
2023-09-18 13:24:55 +0530

    redefine routes

--- app/controllers/splash_screens_controller.rb
+++ app/controllers/splash_screens_controller.rb
@@ -1,8 +1,3 @@
 class SplashScreensController < ApplicationController
-  before_action :set_splash_screen, only: %i[ show edit update destroy ]
-
-  # GET /splash_screens or /splash_screens.json
-  def index
-  end
-
+  def index; end
 end
