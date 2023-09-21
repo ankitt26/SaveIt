f27a2c7a96790077aea9ca278beaf8a13bded673
Ankit <ankit26kushwa@gmail.com>
2023-09-22 00:49:29 +0530

    Show only current user data

--- app/controllers/spends_controller.rb
+++ app/controllers/spends_controller.rb
@@ -1,12 +1,12 @@
 class SpendsController < ApplicationController
   before_action :set_spend, only: %i[show edit update destroy]
 
-  # GET /spends or /spends.json
-  def index
-    c_id = params[:category_id]
-    @category = Category.find(c_id)
-    @spends = @category.spends.order(created_at: :desc)
-  end
+# GET /spends or /spends.json
+def index
+  c_id = params[:category_id]
+  @category = Category.find(c_id)
+  @spends = @category.spends.where(user_id: current_user.id).order(created_at: :desc)
+end
 
   # GET /spends/new
   def new
