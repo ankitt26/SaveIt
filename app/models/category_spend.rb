1bb750680973ed736775f5d9051bda7a74eed5f2
Ankit <ankit26kushwa@gmail.com>
2023-09-22 01:38:39 +0530

    Add validations in models

--- app/models/category_spend.rb
+++ app/models/category_spend.rb
@@ -1,4 +1,7 @@
 class CategorySpend < ApplicationRecord
   has_many :spends
   has_many :categories
+
+  validates :category, presence: true
+  validates :spend, presence: true
 end
--- app/models/spend.rb
+++ app/models/spend.rb
@@ -1,4 +1,7 @@
 class Spend < ApplicationRecord
   has_and_belongs_to_many :categories
   belongs_to :user
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
