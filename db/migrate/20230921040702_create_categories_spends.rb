295269f49b017c2efd321846b1c307a1c423e1b0
Ankit <ankit26kushwa@gmail.com>
2023-09-21 09:42:14 +0530

    create migration CreateCategoriesSpends

---
+++ db/migrate/20230921040702_create_categories_spends.rb
@@ -0,0 +1,10 @@
+class CreateCategoriesSpends < ActiveRecord::Migration[7.0]
+  def change
+    create_table :categories_spends do |t|
+      t.references :category, null: false, foreign_key: true
+      t.references :spend, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_144306) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_040702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,13 +21,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_144306) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories_spends", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "spend_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categories_spends_on_category_id"
+    t.index ["spend_id"], name: "index_categories_spends_on_spend_id"
+  end
+
   create_table "spends", force: :cascade do |t|
     t.string "author_id", null: false
     t.string "name", null: false
     t.integer "amount", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_144306) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories_spends", "categories"
+  add_foreign_key "categories_spends", "spends"
 end
