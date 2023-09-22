613264d7b9951ddc57995484b51e38cfcb688a7d
Ankit <ankit26kushwa@gmail.com>
2023-09-22 13:59:26 +0530

    Write unit test for spend model

---
+++ spec/models/spend_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Spend, type: :model do
+
+    before(:each) do 
+      @user = User.new(name: "Ankit", email: "abc@gmail.com", password: "asd4f3g")
+     @spend = Spend.create(user: @user, name: "cycle", amount: 300)
+    end
+  
+    context "presence validations" do
+  
+      it 'is not valid without a user' do
+        @spend.user_id = nil
+        expect(@spend).to_not be_valid
+      end
+  
+      it 'is not valid without a name' do
+        @spend.name = nil
+        expect(@spend).to_not be_valid
+      end
+  
+      it 'is not valid without an amount' do
+        @spend.amount = nil
+        expect(@spend).not_to be_valid
+      end
+    end
+
+  end
