89fac01ea941060765bca05f9350f96cb13dea45
Ankit <ankit26kushwa@gmail.com>
2023-09-22 11:50:30 +0530

    Write unit test for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before(:each) do
+    @user = User.new(name: "Ankit", email: "abc@gmail.com", password: "asd4f3g")
+  end
+
+  context 'presence validations' do
+    it 'is valid without a name' do
+      @user.name = nil
+      expect(@user).to be_valid
+    end
+
+    it 'is not valid without an email' do
+      @user.email = nil
+      expect(@user).not_to be_valid
+    end
+
+    it 'is not valid without a password' do
+      @user.password = nil
+      expect(@user).not_to be_valid
+    end
+  end
+end
