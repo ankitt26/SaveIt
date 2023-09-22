0b1ada2061b1777f11f8f624b27c8c55f5d9d308
Ankit <ankit26kushwa@gmail.com>
2023-09-22 15:41:09 +0530

    Write unit test for Category_spend model

---
+++ spec/models/category_spend_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe CategorySpend, type: :model do
+  before(:each) do 
+    @user = User.create(name: "Ankit", email: "abc@gmail.com", password: "asd4f3g")
+   @category = Category.create(user: @user, name: "Shopping", icon: "https://rb.gy/2jgmv")
+   @spend = Spend.create(user: @user, name: "cycle", amount: 300)
+   @category_spend = CategorySpend.create(category: @category, spend: @spend)
+  end
+
+  context "presence validations" do
+
+    it 'is not valid without a category' do
+      @category_spend.category_id = nil
+      expect(@category_spend).to_not be_valid
+    end
+
+    it 'is not valid without a spend' do
+      @category_spend.spend_id = nil
+      expect(@category_spend).to_not be_valid
+    end
+  end
+end
