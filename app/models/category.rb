f3f4e79e8d8c405e4a36ee3cf37c6bbf47d96467
Ankit <ankit26kushwa@gmail.com>
2023-09-23 17:25:10 +0530

    Make required changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,6 @@ class Category < ApplicationRecord
   has_many :category_spends, class_name: 'CategorySpend'
   has_many :spends, through: :category_spends
   validates :name, presence: true
+  validates :icon, presence: true
 end
+
